81994c8633d1dca3269b5f2be241ea863533d2c1
Siew Hooi Thong <sh_thong@outlook.com>
2019-04-30 15:46:09 +0800

    Updated/added unit tests for SimpleUriHandler

--- handlers.Uri.UnitTests/UnitTests.cs
+++ handlers.Uri.UnitTests/UnitTests.cs
@@ -1,7 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using NUnit.Framework;
 using Synapse.Core;
+using Synapse.Core.Utilities;
+using Newtonsoft.Json;
+using YamlDotNet.Serialization;
+using System.Xml;
+using System.Xml.Linq;
 
 namespace handlers.Uri.UnitTests
 {
@@ -27,6 +33,9 @@ namespace handlers.Uri.UnitTests
             SimpleUriHandlerParameters parms = new SimpleUriHandlerParameters() { Uri = uri, Format = format };
             string result = handler.GetFileUri( parms.ParsedUri.ToString() );
 
+            string expectedResult = File.ReadAllText( uri );
+            Assert.AreEqual( expectedResult, result );
+
             object o = handler.FormatData( result, parms.Format );
         }
 
@@ -35,6 +44,7 @@ namespace handlers.Uri.UnitTests
         [TestCase( "yaml_in.yaml", ReturnFormat.Yaml )]
         [TestCase( "json_in.json", ReturnFormat.Json )]
         [TestCase( "xml_in.xml", ReturnFormat.Xml )]
+        // ensure Server service is started or the share name will not be available
         public void FileAsUncPath(string file, ReturnFormat format)
         {
             SimpleUriHandler handler = new SimpleUriHandler();
@@ -42,6 +52,9 @@ namespace handlers.Uri.UnitTests
 
             SimpleUriHandlerParameters parms = new SimpleUriHandlerParameters() { Uri = uri, Format = format };
             string result = handler.GetFileUri( parms.ParsedUri.ToString() );
+
+            string expectedResult = File.ReadAllText( uri );
+            Assert.AreEqual( expectedResult, result );
 
             object o = handler.FormatData( result, parms.Format );
         }
@@ -59,7 +72,70 @@ namespace handlers.Uri.UnitTests
             SimpleUriHandlerParameters parms = new SimpleUriHandlerParameters() { Uri = uri, Format = format };
             string result = handler.GetFileUri( parms.ParsedUri.ToString() );
 
+            string expectedResult = File.ReadAllText( Path.Combine( Environment.CurrentDirectory, file ) );
+            Assert.AreEqual( expectedResult, result );
+
             object o = handler.FormatData( result, parms.Format );
+        }
+        [Test]
+        [Category( "File" )]
+        [TestCase( "yaml_in.yaml", ReturnFormat.Yaml )]
+        [TestCase( "json_in.json", ReturnFormat.Json )]
+        [TestCase( "xml_in.xml", ReturnFormat.Xml )]
+        public void MergeData(string file, ReturnFormat format)
+        {
+            SimpleUriHandler handler = new SimpleUriHandler();
+            string uri = Path.Combine( Environment.CurrentDirectory, file );
+
+            SimpleUriHandlerParameters parms = new SimpleUriHandlerParameters() { Uri = uri, Format = format };
+            string uriContent = handler.GetFileUri( parms.ParsedUri.ToString() );
+
+            object inputData = handler.FormatData( uriContent, parms.Format );
+
+            if( format == ReturnFormat.Xml )
+            {
+                XmlDocument xmlDoc = new XmlDocument();
+                xmlDoc.LoadXml( $@"<PXmlDoc><PNode1>PValue1_file_newvalue</PNode1></PXmlDoc>" );
+                handler.MergeData( ref inputData, xmlDoc, format );
+            }
+            else
+            {
+                Dictionary<object, object> merge = new Dictionary<object, object>();
+                merge.Add( "PNode1", "PValue1_file_newvalue" );
+                handler.MergeData( ref inputData, merge, format );
+            }
+
+            string result = null;
+            string expectedResult = null;
+            switch( format )
+            {
+                case ReturnFormat.Json:
+                    {
+                        result = JsonConvert.SerializeObject( inputData, Newtonsoft.Json.Formatting.Indented );
+                        expectedResult = File.ReadAllText( Path.Combine( Environment.CurrentDirectory, $"{format}_out_mergeData.{format}" ) );
+                        object o = JsonConvert.DeserializeObject( expectedResult );
+                        expectedResult = JsonConvert.SerializeObject( o, Newtonsoft.Json.Formatting.Indented );
+                        break;
+                    }
+                case ReturnFormat.Yaml:
+                    {
+                        Serializer serializer = new Serializer();
+                        result = serializer.Serialize( inputData );
+                        expectedResult = File.ReadAllText( Path.Combine( Environment.CurrentDirectory, $"{format}_out_mergeData.{format}" ) );
+                        Dictionary<object, object> o = YamlHelpers.Deserialize( expectedResult );
+                        expectedResult = YamlHelpers.Serialize( o );
+                        break;
+                    }
+                case ReturnFormat.Xml:
+                    { 
+                        result = XmlHelpers.Serialize<string>( inputData );
+                        XmlDocument expectedResultXml = new XmlDocument();
+                        expectedResultXml.Load( Path.Combine( Environment.CurrentDirectory, $"{format}_out_mergeData.{format}" ) );
+                        expectedResult = XmlHelpers.Serialize<string>( expectedResultXml );
+                        break;
+                    }
+            };
+            Assert.AreEqual( expectedResult, result );
         }
     }
 }
